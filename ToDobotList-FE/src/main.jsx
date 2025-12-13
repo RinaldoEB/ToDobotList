ef5dd37f6eacf88db85e046ed5d559c3e36f8c2c
RinaldoEB <rinaldoedbm@gmail.com>
2025-12-13 22:46:07 +0700

    membuat route dan FE react

---
+++ ToDobotList-FE/src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import './index.css'
+import App from './App.jsx'
+import Login from './pages/login.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>s
+      <Routes>
+        <Route path='/' element ={<App/>}/>
+        <Route path='/login' element ={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
---
+++ ToDobotList-FE/src/pages/login.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+const loginPage = () => {
+    return (
+        <>
+            <h1>ini login</h1>
+        </>
+    )
+}
+
+export default loginPage
