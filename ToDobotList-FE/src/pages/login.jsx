4867b73a0e7ae3ae82a749c26cc3716169d0c416
RinaldoEB <rinaldoedbm@gmail.com>
2025-12-14 23:07:37 +0700

    menambahkan register

--- ToDobotList-FE/src/pages/login.jsx
+++ ToDobotList-FE/src/pages/login.jsx
@@ -2,6 +2,10 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const loginPage = () => {
+    const [newName, setNewName] = useState("")
+    const [newEmail, setNewEmail] = useState("")
+    const [newPassword, setNewPassword] = useState("")
+    const [name,setName] = useState("")
     const [email,setEmail] = useState("")
     const [password,setPassword] = useState("")
     const [error, setError] = useState("")
@@ -29,6 +33,39 @@ const loginPage = () => {
         localStorage.setItem("name", data.name)
         navigate('/')
     }
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        const body = {
+            name : newName,
+            email : newEmail,
+            password : newPassword
+        }
+
+        const res = await fetch('http://localhost:3007/api/user/register', {
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify(body)
+        })
+
+        const data = await res.json()
+        
+        if(!res.ok) {
+            console.log(data.message)
+            return
+        }
+        if(res.status === 500) {
+            alert(data.message)
+        }else {
+            alert(`data berhasil di tambah`)
+        }
+
+       setNewName("")
+       setNewEmail("")
+       setNewPassword("")
+    }
     
     return (
         <>
@@ -48,6 +85,36 @@ const loginPage = () => {
                     placeholder="password"
                     value={password} 
                     onChange={e => setPassword(e.target.value)}
+                    required
+                    />
+
+                    <input type="submit" />
+                </form>
+
+                <button>U Dont Have Account ?</button>
+
+                <form onSubmit={handleRegister}>
+                    {error && <p style={{ color : "red" }}>{error}</p>}
+                    <h1>REGISTER</h1>
+
+                    <input type="text" 
+                    placeholder="name" 
+                    value={newName}
+                    onChange={e => setNewName(e.target.value)}
+                    required
+                    />
+
+                    <input type="email" 
+                    placeholder="Email" 
+                    value={newEmail}
+                    onChange={e => setNewEmail(e.target.value)}
+                    required
+                    />
+
+                    <input type="password" 
+                    placeholder="password"
+                    value={newPassword}
+                    onChange={e => setNewPassword(e.target.value)}
                     required
                     />
 
