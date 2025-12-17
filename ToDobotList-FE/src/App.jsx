1afd0f4e1a92c59c8e4e591df4bcfe0b4cccea08
rinaldoeb <rinaldoedbm@gmail.com>
2025-12-17 22:28:24 +0700

    update logic fetch data from BE list note and fix error

--- ToDobotList-FE/src/App.jsx
+++ ToDobotList-FE/src/App.jsx
@@ -4,19 +4,45 @@ import { useNavigate } from 'react-router-dom'
 function App() {
   const navigate = useNavigate()
   const [nama,setNama] = useState("")
-  useEffect(()=> {
-    const token = localStorage.getItem("token")
-    const savedNama = localStorage.getItem("name")
+  const [notes,setNote] = useState([])
+  const token = localStorage.getItem("token")
+  const savedNama = localStorage.getItem("name")
+
+  const listNotes = async() => {
+    const noteUrl = "http://localhost:3007/api/note"
+    const response = await fetch(noteUrl,{  
+      method : "GET",
+      headers : {
+        Authorization : `Bearer ${token}`,
+        "Content-Type" : "application/json"
+      },
+    })
+    const data = await response.json();
+    setNote(data.data)
+
+  }
+  useEffect(()=> {  
     if(!token) {
       navigate('/login')
+      return
     }else {
       setNama(savedNama)
-    }
+      listNotes()
+    } 
   },[])
   return (
     <>
       <div>
         <h1 className='text-indigo-600'>HELLO, {nama}</h1>
+        <h2>List Note</h2>
+        {notes.map(note => (
+            <ul key={note.id}>
+              <li>
+                title : {note.title} <br />
+                content : {note.content}
+              </li>
+            </ul>
+        ))}
       </div>
     </>
   )
